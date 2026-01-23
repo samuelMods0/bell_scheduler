63f19c7d7e890717308ff5a52d14aebe5643c1ec
Samuel <orsleyway@gmail.com>
2026-01-23 08:35:47 -0800

    fixing urls

--- quasimoto/main/urls.py
+++ quasimoto/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("test/", views.test, name="test"),
+    path("", views.index, name="index.html"),
+    path("test/", views.test, name="test.html"),
 ]
